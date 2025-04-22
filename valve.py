a02c32a66aaccf9c07f6b3ffce1c3d275d783804
Kevin <kazamalexis@icloud.com>
2025-04-22 13:48:14 -0400

    fully integrating hardware, getting correct NDVI images

--- valve.py
+++ valve.py
@@ -2,23 +2,26 @@
 
 import RPi.GPIO as GPIO
 import time 
-
-def off():
-    init()
-    GPIO.output(16,False)
-    GPIO.output(18,False)
-    
+  
 def init():
     GPIO.setmoode(GPIO.BOARD)
     GPIO.setup(16,GPIO.OUT)
     GPIO.setup(18,GPIO.OUT)
 
+def off():
+    init()
+    GPIO.output(16,False)
+    GPIO.output(18,False)
+  
+
+#open valve
 def open(sec):
     init()
     GPIO.output(16,True)
     GPIO.output(18,False)
     time.sleep(sec)
 
+#close valve
 def close():
     init()
     GPIO.output(16,False)
