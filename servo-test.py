3a1cdf559975ad335c06b49b305220c6dab29a84
AboveUnderscores <devalva216@gmail.com>
2025-10-28 17:03:45 -0400

    editing branch for new hardware setup

---
+++ servo-test.py
@@ -0,0 +1,21 @@
+import time
+import RPi.GPIO as GPIO
+
+def setAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(8, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)  # Time in seconds
+    GPIO.output(8, False)
+    pwm.ChangeDutyCycle(0)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+pwm = GPIO.PWM(8, 50)
+pwm.start(0)
+
+setAngle(10)
+time.sleep(1)
+
+setAngle(180)
+time.sleep(1)
